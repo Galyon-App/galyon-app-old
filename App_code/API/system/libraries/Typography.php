1d37c7aca13a74dbf2c2ea5c04fec2216d78737d
BytesCrafter <bytescrafter@gmail.com>
2021-03-05 01:18:48 +0800

    Fixed something on the CI system on inserted code.

--- App_code/API/system/libraries/Typography.php
+++ App_code/API/system/libraries/Typography.php
@@ -242,15 +242,15 @@ class CI_Typography {
 						// If the user submitted their own paragraph tags within the text
 						// we will retain them instead of using our tags.
 						'/(<p[^>*?]>)<p>/'	=> '$1', // <?php
-/*
-  Authors : initappz (Rahul Jograna)
-  Website : https://initappz.com/
-  App Name : ionic 5 groceryee app
-  Created : 10-Sep-2020
-  This App Template Source code is licensed as per the
-  terms found in the Website https://initappz.com/license
-  Copyright and Good Faith Purchasers © 2020-present initappz.
-*/ BBEdit syntax coloring bug fix
+						/*
+						Authors : initappz (Rahul Jograna)
+						Website : https://initappz.com/
+						App Name : ionic 5 groceryee app
+						Created : 10-Sep-2020
+						This App Template Source code is licensed as per the
+						terms found in the Website https://initappz.com/license
+						Copyright and Good Faith Purchasers © 2020-present initappz.
+						*/ 
 
 						// Reduce multiple instances of opening/closing paragraph tags to a single one
 						'#(</p>)+#'			=> '</p>',
