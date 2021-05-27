54be82a02cfe4ecf7e7f74f155a9bcdb6c9231d1
BytesCrafter <bytescrafter@gmail.com>
2021-05-28 00:39:05 +0800

    Migrated components from store app to single app.

---
+++ Source/Mobile/src/app/interfaces/interfaces.ts
@@ -0,0 +1,11 @@
+/*
+  Name: Galyon App
+  Authors : Bytes Crafter
+  Website : https://bytescrafter.net
+  Created : 01-Jan-2021
+*/
+export interface Components {
+  icon: string,
+  name: string,
+  redirecTo: string;
+}
---
+++ Source/Mobile/src/app/interfaces/register.ts
@@ -0,0 +1,16 @@
+/*
+  Name: Galyon App
+  Authors : Bytes Crafter
+  Website : https://bytescrafter.net
+  Created : 01-Jan-2021
+*/
+export interface register {
+    email: string;
+    first_name: string;
+    last_name: string;
+    gender: string;
+    phone: string;
+    address: string;
+    avatar: string;
+    password: string;
+}
