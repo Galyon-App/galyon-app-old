1ec0770810726d44b5880d940240cf60298500e3
BytesCrafter <bytescrafter@gmail.com>
2021-08-21 22:08:02 +0800

    Update the session config and also enable migration config and directory.

--- Source/Backend/application/config/migration.php
+++ Source/Backend/application/config/migration.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | and disable it back when you're done.
 |
 */
-$config['migration_enabled'] = FALSE;
+$config['migration_enabled'] = TRUE;
 
 /*
 |--------------------------------------------------------------------------
@@ -75,7 +75,7 @@ $config['migration_auto_latest'] = FALSE;
 | be upgraded / downgraded to.
 |
 */
-$config['migration_version'] = 0;
+$config['migration_version'] = 20210818202900;
 
 /*
 |--------------------------------------------------------------------------
