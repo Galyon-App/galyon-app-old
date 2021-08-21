cf667e952b14cc5bbb8ff81bf93c9ad2aace2593
BytesCrafter <bytescrafter@gmail.com>
2021-08-21 22:15:57 +0800

    Added user authentication via the api in the backend using jwt.

--- Source/Backend/application/controllers/galyon/v1/Users.php
+++ Source/Backend/application/controllers/galyon/v1/Users.php
@@ -14,7 +14,7 @@ class Users extends Galyon_controller {
     function __construct(){
 		parent::__construct();
     }
-
+    
     function login() {
         $uname = $this->input->post('uname');
         $pword = $this->input->post('pword');
@@ -26,50 +26,25 @@ class Users extends Galyon_controller {
         $user = $this->Crud_model->get('users', '*', array( "email" => $uname ) );
         if($user != null) {
             if(password_verify($pword, $user->password)){
-                unset($user->password);
-                $this->session->set_userdata(array(
-                    'username' => $user->mobile,
+                //Finalized
+                $now = strtotime(get_my_local_time());
+                $span = 43200; ////12hrs
+                $expiry = (int)$now + (int)$span;
+
+                $user_object = array(
+                    'uuid' => $user->uuid,
                     'email' => $user->email,
                     'role' => $user->type,
-                    'token' => session_id(),
-                    'logged_in' => true
-                ));
-                $this->json_response($this->session->userdata()); //session_id()
+                    'logged_in' => true,
+                    'expiry' => $expiry
+                );
+                $token = JWT::encode($user_object, $this->config->item('encryption_key'));
+                $this->json_response($token);
             } else {
                 $this->json_response(null, false, "Invalid credential!");
             }
-            
         } else {
             $this->json_response(null, false, "User not found!");
         }
-    }
-
-    function verify() {
-        $logged_in = $this->session->userdata('logged_in');
-        $token = $this->session->userdata('token');
-
-        if($token != $this->input->post('token')) {
-            $this->json_response(null, false, "Session is invalid.");
-        }
-        if(!$logged_in) {
-            $this->json_response(null, false, "You are not logged in.");
-        }
-        
-        $this->json_response($this->session->userdata());
-    }
-
-    function logout() {
-        $logged_in = $this->session->userdata('logged_in');
-        $token = $this->session->userdata('token');
-
-        if($token != $this->input->post('token')) {
-            $this->json_response(null, false, "Session is invalid.");
-        }
-        if(!$logged_in) {
-            $this->json_response(null, false, "You are not logged in.");
-        }
-
-        $this->session->sess_destroy();
-        $this->json_response(null);
     }
 }
