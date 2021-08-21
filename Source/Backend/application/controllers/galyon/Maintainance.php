25a206e0406fca89303d00938bb9e2c937f50cf9
BytesCrafter <bytescrafter@gmail.com>
2021-08-21 22:13:10 +0800

    Updated the new stable and scalable structure for database in the backend.

---
+++ Source/Backend/application/controllers/galyon/Maintainance.php
@@ -0,0 +1,118 @@
+<?php
+
+/*
+  Name: Galyon App
+  Authors : Bytes Crafter
+  Website : https://bytescrafter.net
+  Created : 01-Jan-2021
+*/
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+require_once APPPATH.'/core/Galyon_controller.php';
+
+class Maintainance extends Galyon_controller
+{
+    function __construct() {
+        parent::__construct();
+        //TODO: Access only admin.
+    }
+
+    public function execute() {
+        //Execute all fixing here.
+        $user_uuid = $this->add_uuidto_users();
+        $store_uuid = $this->update_store_owner_uuid();
+        $products_uuid = $this->update_products_uuid();
+        $orders_uuid = $this->update_orders_uuid(); 
+        $addresses_uuid = $this->update_addresses_uuid();
+        $cities_uuid = $this->update_cities_uuid();
+        $categories_uuid = $this->update_categories_uuid();
+
+        //TODO: save utc datetime on setting as last check and fix.
+        $this->json_response(array(
+            "user_uuid" => $user_uuid,
+            "store_uuid" => $store_uuid,
+            "products_uuid" => $products_uuid,
+            "orders_uuid" => $orders_uuid,
+            "addresses_uuid" => $addresses_uuid,
+            "cities_uuid" => $cities_uuid,
+            "categories_uuid" => $categories_uuid,
+        ));
+    }
+
+    protected function update_categories_uuid() {
+        $categories = $this->Crud_model->get_categories_without_uid();
+
+        foreach($categories as $category) {
+            $uuid = $this->uuid->v4();
+            $this->Crud_model->set_category_parent_and_uuid($category, $uuid);
+        }
+
+        return count($categories);
+    }    
+
+    protected function update_cities_uuid() {
+        $cities = $this->Crud_model->get_cities_without_uuid();
+
+        foreach($cities as $city) {
+            $uuid = $this->uuid->v4();
+            $this->Crud_model->set_city_uuid($city->id, $uuid);
+        }
+
+        return count($cities);
+    }
+
+    protected function update_addresses_uuid() {
+        $addresses = $this->Crud_model->get_address_without_owner_and_uuid();
+
+        foreach($addresses as $address) {
+            $uuid = $this->uuid->v4();
+            $this->Crud_model->set_address_uuid($address, $uuid);
+        }
+
+        return count($addresses);
+    }
+
+    protected function update_orders_uuid() {
+        $orders = $this->Crud_model->get_orders_with_uid();
+
+        foreach($orders as $order) {
+            $uuid = $this->uuid->v4();
+            $this->Crud_model->set_order_foreign_and_uuid($order, $uuid);
+        }
+
+        return count($orders);
+    }
+    
+    protected function update_products_uuid() {
+        $pruducts = $this->Crud_model->get_products_with_sid();
+
+        foreach($pruducts as $pruduct) {
+            $uuid = $this->uuid->v4();
+            $this->Crud_model->set_pruduct_store_and_uuid($pruduct->store_id, $uuid);
+        }
+
+        return count($pruducts);
+    }
+
+    protected function update_store_owner_uuid() {
+        $stores = $this->Crud_model->get_stores_with_uid();
+
+        foreach($stores as $store) {
+            $uuid = $this->uuid->v4();
+            $this->Crud_model->set_store_owner_and_uuid($store->owner, $uuid);
+        }
+
+        return count($stores);
+    }
+
+    protected function add_uuidto_users() {
+        $users = $this->Crud_model->get_users_without_uuid();
+
+        foreach($users as $user) {
+            $uuid = $this->uuid->v4();
+            $this->Crud_model->set_user_uuid($user->id, $uuid);
+        }
+
+        return count($users);
+    }
+}
--- Source/Backend/application/models/Crud_model.php
+++ Source/Backend/application/models/Crud_model.php
@@ -12,5 +12,89 @@ class Crud_model extends Galyon_model {
 
 	function __construct(){
 		parent::__construct();
+  }
+
+  function get_users_without_uuid() {
+    return $this->get('users', 'id', array( "uuid" => null ), array( "uuid" => '' ), 'result' );
+  }
+
+  function set_user_uuid($user_id, $new_uuid) {
+    return $this->update('users', array( "uuid" => $new_uuid), array( "id" => $user_id ) );
+  }
+
+  function get_stores_with_uid() {
+    return $this->get('stores', 'owner', 'CHAR_LENGTH(owner) < 36', array('owner'=>null), 'result' );
+  }
+
+  function set_store_owner_and_uuid($uid, $uuid) {
+    $user = $this->get('users', 'uuid', array( "id" => $uid) );
+    if($user) {
+      return $this->update('stores', array("uuid" => $uuid, "owner" => $user->uuid), array( "owner" => $uid ) );
+    } else {
+      return true;
     }
+  }
+
+  function get_products_with_sid() {
+    return $this->get('products', 'store_id', 'CHAR_LENGTH(store_id) < 36', array('store_id'=>null), 'result' );
+  }
+
+  function set_pruduct_store_and_uuid($uid, $uuid) {
+    $store = $this->get('stores', 'uuid', array( "id" => $uid) );
+    if($store) {
+      return $this->update('products', array("uuid" => $uuid, "store_id" => $store->uuid), array( "store_id" => $uid ) );
+    } else {
+      return true;
+    }
+  }
+
+  function get_orders_with_uid() {
+    return $this->get('orders', 'id, uid, store_id, driver_id', 'CHAR_LENGTH(uid) < 36', array('uid'=>null), 'result' );
+  }
+
+  function set_order_foreign_and_uuid($order, $uuid) {    
+    $driver = $this->get('users', 'uuid', array( "id" => $order->driver_id, "type" => "driver") );
+    if($driver) {
+      $this->update('orders', array("driver_id" => $driver->uuid), array( "id" => $order->id ) );
+    }
+    
+    $user = $this->get('users', 'uuid', array( "id" => $order->uid) ) 
+      ? $this->get('users', 'uuid', array( "id" => $order->uid) ) : NULL;
+    $store = $this->get('stores', 'uuid', array( "id" => $order->store_id) )
+      ? $this->get('stores', 'uuid', array( "id" => $order->store_id) ) : NULL;
+    return $this->update('orders', array("uid" => $user->uuid, "store_id" => $store->uuid), array( "id" => $order->id ) );
+  }
+
+  function get_cities_without_uuid() {
+    return $this->get('cities', 'id', array( "uuid" => null ), array( "uuid" => '' ), 'result' );
+  }
+
+  function set_city_uuid($city_id, $new_uuid) {
+    return $this->update('cities', array( "uuid" => $new_uuid), array( "id" => $city_id ) );
+  }
+
+  function get_address_without_owner_and_uuid() {
+    return $this->get('address', 'id, uid, store_id', array( "uuid" => null ), array( "uuid" => '' ), 'result' );
+  }
+
+  function set_address_uuid($address, $new_uuid) {
+    $user = $this->get('users', 'uuid', array( "id" => $address->uid) ) 
+      ? $this->get('users', 'uuid', array( "id" => $address->uid) ) : NULL;
+    $store = $this->get('stores', 'uuid', array( "id" => $address->store_id) ) 
+      ? $this->get('stores', 'uuid', array( "id" => $address->store_id) ) : NULL;
+    return $this->update('address', array( "uuid" => $new_uuid, "uid" => $user->uuid, "store_id" => $store->uuid), array( "id" => $address->id ) );
+  }
+
+  function get_categories_without_uid() {
+    return $this->get('category', 'id, parent_id', 'CHAR_LENGTH(uuid) < 36', array('uuid'=>null), 'result' );
+  }
+
+  function set_category_parent_and_uuid($category, $uuid) {
+    $parent = $this->get('category', 'uuid', array( "id" => $category->parent_id) );
+    if($parent) {
+      return $this->update('category', array("uuid" => $uuid, "parent_id" => $parent->uuid), array( "id" => $category->id ) );
+    } else {
+      return $this->update('category', array("uuid" => $uuid), array( "id" => $category->id ) );
+    }
+  }
 }
